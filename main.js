0bf80e80485eca8988c013046adbc5f29db884a7
Sim Gill <simgill89@gmail.com>
2017-02-09 14:32:26 -0800

    working prototype

--- main.js
+++ main.js
@@ -2,55 +2,71 @@ var state = {
   currentSection : "politics",
   submitted : false,
   rawData: [],
-  articles: [],
+  currentURL: function(){
+    return "https://api.nytimes.com/svc/topstories/v2/" + this.currentSection + ".json?api-key=3047b4f43aa340b1a9976a2cefadbf7f";
+  }
 }
-
-var baseUrl = "https://api.nytimes.com/svc/topstories/v2/";
-var endUrl = '.json?api-key=3047b4f43aa340b1a9976a2cefadbf7f';
-var fullUrl = baseUrl + state.currentSection + endUrl;
-//category = input.val()
 
 function getData(callback){
   $.ajax({
-    url: fullUrl,
+    url: state.currentURL(),
     method: 'GET',
     success: callback
   });
 }
 
+
+
 function addToState(data){
   state.rawData = data.results;
-  console.log(state.rawData);
-  // data.results.forEach(function(article){
-  //   var articleObj = {
-  //     abstract : article.abstract || '',
-  //     byline : article.byline || '',
-  //     published_date : article.published_date || '',
-  //     url: article.url || '',
-  //     title: article.title || '',
-  //     thumbnail: article.multimedia[3].url || '',
-  //     thumbCaption: article.multimedia[3].caption || '',
-  //     jumboThumb: article.multimedia[4].url || '',
-  //     jumboCaption: article.multimedia[4].caption || ''
-  //   };
-    //console.log(articleObj);
-    //state.articles.push(articleObj);
-  //})
-  //console.log(state.articles);
-  // console.log(data);
-  // console.log(data.results[0].abstract);
-  // console.log(data.results[0].byline);
-  // console.log(data.results[0].published_date);
-  // console.log(data.results[0].url);
-  // console.log(data.results[0].title);
-  // console.log(data.results[0].multimedia[3].url);
-  // console.log(data.results[0].multimedia[3].caption);
-  // console.log(data.results[0].multimedia[4].url);
-  // console.log(data.results[0].multimedia[4].caption);
 }
 
-function display(data){
+function display(state) {
+   $('.listOfArticles').empty();
+    state.rawData.forEach(function(article) {
+        var list = `<div> ${article.title} ${article.byline}</div>`
+        $('.listOfArticles').append(list);
+    });
 
 }
 
-$(getData(addToState));
+
+
+
+function init(data){
+  addToState(data);
+  display(state);
+}
+
+
+
+
+
+
+function eventListener(){
+$('#search').submit(function(e) {
+        e.preventDefault();
+        state.currentSection = $('#selection').val();
+        console.log(state.currentURL());
+        getData(init);
+    });
+}
+
+
+
+
+
+$(eventListener);
+  
+
+
+
+
+
+
+
+
+
+
+
+
