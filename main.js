a153d431651a412922871162baf4bf768af172fc
Terry Hayes <terrychayes@gmail.com>
2017-02-09 16:07:01 -0500

    Changes addToState function to add all raw article objects to state vs only parts we will use.

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log('test');
+var state = {
+  currentSection : "politics",
+  submitted : false,
+  rawData: [],
+  articles: [],
+}
+
+var baseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+var endUrl = '.json?api-key=3047b4f43aa340b1a9976a2cefadbf7f';
+var fullUrl = baseUrl + state.currentSection + endUrl;
+//category = input.val()
+
+function getData(callback){
+  $.ajax({
+    url: fullUrl,
+    method: 'GET',
+    success: callback
+  });
+}
+
+function addToState(data){
+  state.rawData = data.results;
+  console.log(state.rawData);
+  // data.results.forEach(function(article){
+  //   var articleObj = {
+  //     abstract : article.abstract || '',
+  //     byline : article.byline || '',
+  //     published_date : article.published_date || '',
+  //     url: article.url || '',
+  //     title: article.title || '',
+  //     thumbnail: article.multimedia[3].url || '',
+  //     thumbCaption: article.multimedia[3].caption || '',
+  //     jumboThumb: article.multimedia[4].url || '',
+  //     jumboCaption: article.multimedia[4].caption || ''
+  //   };
+    //console.log(articleObj);
+    //state.articles.push(articleObj);
+  //})
+  //console.log(state.articles);
+  // console.log(data);
+  // console.log(data.results[0].abstract);
+  // console.log(data.results[0].byline);
+  // console.log(data.results[0].published_date);
+  // console.log(data.results[0].url);
+  // console.log(data.results[0].title);
+  // console.log(data.results[0].multimedia[3].url);
+  // console.log(data.results[0].multimedia[3].caption);
+  // console.log(data.results[0].multimedia[4].url);
+  // console.log(data.results[0].multimedia[4].caption);
+}
+
+function display(data){
+
+}
+
+$(getData(addToState));
