0cede36e237150791bdf1e696e6e43e4d941cbeb
Terry Hayes <terrychayes@gmail.com>
2017-02-10 16:36:56 -0500

    Adds flexbox styling; additional categories.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//This is the NYT-flex branch
-
 var state = {
   currentSection : "",
   submitted : false,
@@ -9,7 +7,6 @@ var state = {
   }
 }
 
-
 function getData(callback){
   $.ajax({
     url: state.currentURL(),
@@ -18,11 +15,9 @@ function getData(callback){
   });
 }
 
-
 function addToState(data){
   state.rawData = data.results;
 }
-
 
 function display(state) {
    $('.container').empty();
@@ -30,25 +25,21 @@ function display(state) {
     state.rawData.forEach(function(article) {
         var list = `<div class="article-div">
                       <img src="NYTlogo.png" alt="NYT Logo" class="logo-img">
-                      <h2>${article.title}</h2>
+                      <h3>${article.title}</h3>
                       <p>${article.byline}<br>${article.published_date}</p>
-                      <ul>
-                        <h5>${article.abstract}</h5>
-                        <br>
-                        <a href="${article.url}">Full Text Article</a>
-                      </ul>
+                      <div class="inner">
+                        <h5>${article.abstract} <a href="${article.url}">More...</a></h5>
+                      </div>
                     </div>`
         $('.container').append(list);
     });
     state.rawData=[];
 }
 
-
 function init(data){
   addToState(data);
   display(state);
 }
-
 
 function eventListener(){
   $('#search').submit(function(e) {
